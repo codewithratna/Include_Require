1d470e9f3f7a3f0b3d8111e1f37a4ff365c29950
ratnaphp <ra7709702@gmail.com>
2025-05-14 16:00:25 +0600

    project

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include ("head.php");?>
+</head>
+<body>
+    <?php include_once'header.php';?>
+</body>
+      <?php include("home.php"); ?>
+</html>
+<footer>
+  <!-- footer content here -->
+   <?php include ("footer.php");?>
+</footer>
 
-
-?>
